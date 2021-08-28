e56d6012f89031a30f6c0c73934900af3d6d443e
kirrog <rroogg76aacchh@gmail.com>
2021-08-28 19:13:17 +0300

    Added multithreading in dataset_work

--- src/dataset_work.py
+++ src/dataset_work.py
@@ -8,7 +8,7 @@ from threading import Thread
 masks_path = '../dataset/results'
 img_x, img_y = 512, 512
 res_path = '../dataset/hard_cases'
-
+treads_number = 8
 
 def green2rgb(image):
     res = np.zeros((image.shape[0], image.shape[1], 4))
@@ -48,28 +48,96 @@ def load_masks(num):
 def load_gener(num):
     return np.load(masks_path + '/gener_' + str(num) + '.npy')
 
+#-------------------------------------------------------------
+
+class XorTransformWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            result, iter, full_image, deleter_image =  self.queue.get()
+            try:
+                result[iter] = xor_images(full_image, deleter_image)
+            finally:
+                sys.stdout.write("\rImage %i transformed" % iter)
+                self.queue.task_done()
+
 
 def xor_image_sets(full, deleter):
     result = np.zeros((len(full), img_x, img_y, 1), dtype=np.float16)
+    queue = Queue()
+    for x in range(treads_number):
+        worker = XorTransformWorker(queue)
+        worker.daemon = True
+        worker.start()
     for full_image, deleter_image, iter in zip(full, deleter, range(len(full))):
-        result[iter] = xor_images(full_image, deleter_image)
-        sys.stdout.write("\rImage %i transformed" % iter)
+        queue.put((result, iter, full_image, deleter_image))
+    queue.join()
     return result
 
 
+class G2RGBTransformWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            image, dir, iter = self.queue.get()
+            try:
+                res = green2rgb(image)
+                plt.imsave((res_path + dir + '{:04d}'.format(i) + '.png'), res)
+                sys.stdout.write("\rImage %i transformed" % i)
+            finally:
+                self.queue.task_done()
+
+
 def transform_results_g2rgb(results, dir):
+    queue = Queue()
+    for x in range(treads_number):
+        worker = G2RGBTransformWorker(queue)
+        worker.daemon = True
+        worker.start()
     for image, i in zip(results, range(len(results))):
-        res = green2rgb(image)
-        plt.imsave((res_path + dir + '{:04d}'.format(i) + '.png'), res)
-        sys.stdout.write("\rImage %i transformed" % i)
+        queue.put((image, dir, i))
+    queue.join()
+
+
+class GR2RGBTransformWorker(Thread):
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            # Get the work from the queue and expand the tuple
+            image_g, image_r, i, dir = self.queue.get()
+            try:
+                res = green_and_red2rgb(image_g, image_r)
+                plt.imsave((res_path + dir + '{:04d}'.format(i) + '.png'), res)
+                sys.stdout.write("\rImage %i transformed" % i)
+            finally:
+                self.queue.task_done()
 
 
 def transform_results_gr2rgb(results_g, results_r, dir):
+    queue = Queue()
+    for x in range(treads_number):
+        worker = GR2RGBTransformWorker(queue)
+        worker.daemon = True
+        worker.start()
     for image_r, image_g, i in zip(results_g, results_r, range(len(results_g))):
-        res = green_and_red2rgb(image_g, image_r)
-        plt.imsave((res_path + dir + '{:04d}'.format(i) + '.png'), res)
-        sys.stdout.write("\rImage %i transformed" % i)
+        queue.put((image_g, image_r, i, dir))
+    queue.join()
 
+
+#-------------------------------------------------------------
 
 for i in range(5, 9):
     masks = load_masks(i)
