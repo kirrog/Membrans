389d6d092fdca0bdd6087d4193f1eabb4076d8fd
Kirill <rroogg76aacchh@gmail.com>
2021-11-07 17:12:10 +0300

    Added config logic

--- src/test_hard_cases.py
+++ src/test_hard_cases.py
@@ -1,15 +1,16 @@
 from tensorflow import keras
 
+from src.utils.config_loader import model_path
+from src.utils.config_loader import model_path_logs
+from src.utils.config_loader import output_data_dir
+from src.utils.config_loader import one_test_data_dir
+
 from src.clearer.datasets_loader import load_clearer_dataset_tests, load_test_dataset_from_images_from
 from src.clearer.models.clearer_model_attention_u_net import clearer_model_attention_u_net
 from src.clearer.models.clearer_model_r2u_net import clearer_model_r2u_net
 from src.clearer.models.clearer_model_ru_net import clearer_model_ru_net
 from src.clearer.models.clearer_model_u_net import clearer_model_u_net
 from src.utils.matrix2png_saver import transform_results
-
-model_path = '../models/clearer/clearer_weights.h5'
-model_path_logs = '../models/clearer/logs/r2u_net/ep002-loss0.095-val_loss0.132-accuracy0.955_20211019-183449.h5'
-output_path = 'ru_r2u/r2u/ttlr/'
 
 logs = True
 if logs:
@@ -18,6 +19,6 @@ if logs:
 else:
     model = keras.models.load_model(model_path)
 
-predicts = load_test_dataset_from_images_from("../dataset/test_cases/ttlr/")
+predicts = load_test_dataset_from_images_from(one_test_data_dir)
 res = model.predict(x=predicts, batch_size=2)
-transform_results(res, output_path)
+transform_results(res, output_data_dir)
---
+++ src/utils/config_loader.py
@@ -0,0 +1,22 @@
+config_path = '../../config.config'
+
+with open(config_path) as f:
+    lines = list(f)
+    dict_key_word = []
+    for line in lines:
+        key, word = line.strip().split('=')
+        print(str(key) + ': ' + str(word))
+        dict_key_word.append((key, word))
+    dictionary = dict(dict_key_word)
+    threads_loading = int(dictionary['threads_loading'])
+    cache = int(dictionary['cache'])
+    batch_size = int(dictionary['batch_size'])
+    model_path = dictionary['model_path']
+    model_path_logs = dictionary['model_path_logs']
+    train_data = dictionary['train_data']
+    test_data = dictionary['test_data']
+    valid_data = dictionary['valid_data']
+    one_test_data_dir = dictionary['one_test_data_dir']
+    output_data_dir = dictionary['output_data_dir']
+    output_model_dir = dictionary['output_model_dir']
+
