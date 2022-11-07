55fc3a66b00c84c33483ff415f80df6266f4bfa5
kirrog <kirrog@zertan.ru>
2022-11-07 18:05:43 +0300

    feat: pipeline base

---
+++ src/pipeline.py
@@ -0,0 +1,19 @@
+from typing import List
+
+import numpy as np
+
+
+def init():
+    pass
+
+
+def apply_u_net(case: np.array) -> np.array:
+    pass
+
+
+def apply_generator(case: np.array, orientirs: List[int]) -> np.array:
+    pass
+
+
+def convert2stl_and_save(case: np.array):
+    pass
