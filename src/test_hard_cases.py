7c98df372ad7a9e4d6dfd77d2c7bc779a9509480
kirrog <kirrog@zertan.ru>
2023-02-27 00:08:24 +0300

    feat: fit predict version

--- src/test_hard_cases.py
+++ src/test_hard_cases.py
@@ -1,20 +1,16 @@
 from tensorflow import keras
 
+from src.clearer.datasets_loader import load_test_dataset_from_images_from
+from src.clearer.models.clearer_model_u_net import clearer_model_u_net
 from src.utils.config_loader import model_path
 from src.utils.config_loader import model_path_logs
-from src.utils.config_loader import output_data_dir
 from src.utils.config_loader import one_test_data_dir
-
-from src.clearer.datasets_loader import load_clearer_dataset_tests, load_test_dataset_from_images_from
-from src.clearer.models.clearer_model_attention_u_net import clearer_model_attention_u_net
-from src.clearer.models.clearer_model_r2u_net import clearer_model_r2u_net
-from src.clearer.models.clearer_model_ru_net import clearer_model_ru_net
-from src.clearer.models.clearer_model_u_net import clearer_model_u_net
+from src.utils.config_loader import output_data_dir
 from src.utils.matrix2png_saver import transform_results
 
 logs = True
 if logs:
-    model = clearer_model_r2u_net()
+    model = clearer_model_u_net()
     model.load_weights(model_path_logs)
 else:
     model = keras.models.load_model(model_path)
