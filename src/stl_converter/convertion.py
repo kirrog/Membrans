5ab73cd56d1f01c24f09147c9bd7180cfa04e9ac
kirrog <kirrog@zertan.ru>
2023-06-03 17:44:23 +0300

    feat: add membran projecting from defect voxels

--- src/stl_converter/convertion.py
+++ src/stl_converter/convertion.py
@@ -1,54 +1,94 @@
+from collections import defaultdict
 from pathlib import Path
 
 import numpy as np
 import voxelfuse as vf
 from tqdm import tqdm
 
-min_size = 5
+min_size = 3
 membran_code = 1
 bone_code = 2
 
 
+def coordinate_hex(x: int, y: int, z: int) -> str:
+    return hex(x + y * pow(10, 3) + z * pow(10, 6))
+
+
+class Voxel:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.neighbours = set()
+        self.marked = False
+
+    def __hex__(self):
+        return coordinate_hex(self.x, self.y, self.z)
+
+
+def threshold_voxels_by_cohesion_number(data: np.array,
+                                        component_elements_threshold: int = 100,
+                                        voxels_neighbours_threshold: int = 3) -> np.array:
+    hex2voxel = dict()
+    coords = np.where(data == membran_code)
+    for i, j, k in zip(coords[0], coords[1], coords[2]):
+        hex2voxel[coordinate_hex(i, j, k)] = Voxel(i, j, k)
+    for voxel in hex2voxel.values():
+        for i_i in range(-1, 2):
+            for j_j in range(-1, 2):
+                for k_k in range(-1, 2):
+                    if i_i == 0 and j_j == 0 and k_k == 0:
+                        continue
+                    i, j, k = voxel.x + i_i, voxel.y + j_j, voxel.z + k_k
+                    if data[i, j, k] == membran_code:
+                        neighbour = hex2voxel[coordinate_hex(i, j, k)]
+                        voxel.neighbours.add(neighbour)
+                        neighbour.neighbours.add(voxel)
+    voxel2delete = list(filter(lambda x: len(x.neighbours) <= voxels_neighbours_threshold, hex2voxel.values()))
+    for voxel in voxel2delete:
+        for neighbour in voxel.neighbours:
+            neighbour.neighbours.remove(voxel)
+    voxels_set = {x for x in filter(lambda x: x not in voxel2delete, hex2voxel.values())}
+    components = []
+    while len(voxels_set) > 0:
+        voxel = voxels_set.pop()
+        component_elements = set()
+        front = [voxel]
+        while len(front) > 0:
+            current_voxel = front.pop()
+            if current_voxel in voxels_set:
+                voxels_set.remove(current_voxel)
+            current_voxel.marked = True
+            component_elements.add(current_voxel)
+            for elem in current_voxel.neighbours:
+                if not elem.marked:
+                    front.append(elem)
+        components.append(list(component_elements))
+    filtered_components = list(filter(lambda x: len(x) > component_elements_threshold, components))
+    result_data = np.zeros(data.shape)
+    counter = 0
+    for component in filtered_components:
+        for voxel in component:
+            result_data[voxel.x, voxel.y, voxel.z] = 1
+            counter += 1
+    print(counter)
+    return result_data
+
+
 def find_membran_surface(m_data, b_data):
     merge = m_data * membran_code + b_data * bone_code
-    x_len, y_len, z_len = merge.shape
-    regions = []
-    for i in tqdm(range((x_len // min_size) + (1 if (x_len % min_size != 0) else 0))):
-        for j in range((y_len // min_size) + (1 if (y_len % min_size != 0) else 0)):
-            for k in range((z_len // min_size) + (1 if (z_len % min_size != 0) else 0)):
-                slice = merge[i * min_size:(i + 1) * min_size,
-                        j * min_size:(j + 1) * min_size,
-                        k * min_size:(k + 1) * min_size]
-                if np.sum(slice[slice == membran_code]) > 0:
-                    regions.append((i, j, k, slice))
-    print(len(regions))
-    regions_border = list(filter(lambda x: x[3].mean() != membran_code, regions))
-    print(len(regions_border))
-    regions_border_without_bone = list(filter(lambda x: len(x[3][x[3] == bone_code]) == 0, regions))
+    coords = np.where(merge == membran_code)
+    regions_border_without_bone = []
+    for i, j, k in zip(coords[0], coords[1], coords[2]):
+        slice_data = merge[i - 1:i + 2, j - 1:j + 2, k - 1:k + 2]
+        if slice_data.mean() != membran_code and len(slice_data[slice_data == bone_code]) == 0:
+            regions_border_without_bone.append((i, j, k))
     print(len(regions_border_without_bone))
-    result_points = []
-    for region in tqdm(regions_border_without_bone):
-        i, j, k, slice = region
-        for i_ in range(min_size):
-            for j_ in range(min_size):
-                for k_ in range(min_size):
-                    i_i = i * min_size + i_ - 1
-                    j_j = j * min_size + j_ - 1
-                    k_k = k * min_size + k_ - 1
-                    if i_i < 0 or j_j < 0 or k_k < 0 or i_i > (x_len - 3) or j_j > (y_len - 3) or k_k > (z_len - 3):
-                        continue
-                    slice_ = merge[i_i:i_i + 3,
-                             j_j:j_j + 3,
-                             k_k:k_k + 3]
-                    if slice_.mean() < membran_code and slice_[1, 1, 1] == membran_code:
-                        result_points.append((i_i + 1, j_j + 1, k_k + 1))
-    print(len(regions_border_without_bone) * pow(min_size, 3))
-    print(len(result_points))
     result_matrix = np.zeros(merge.shape)
-    for i, j, k in tqdm(result_points):
+    for i, j, k in regions_border_without_bone:
         result_matrix[i, j, k] = 1
-
-    return result_matrix
+    thresholded_matrix = threshold_voxels_by_cohesion_number(result_matrix)
+    return thresholded_matrix
 
 
 def convert2membran_object(membran_path: Path, bone_path: Path, path_output: Path):
